d545869d7f4f75f61aea3e12ca963d38f6c1641f
yunha <wjh0970@gmail.com>
2022-11-30 20:26:04 +0900

    dart prac_2

--- main.dart
+++ main.dart
@@ -1,8 +1,52 @@
 void main() {
   // data type
-  String name = "wuyu";
-  bool boolean = true;
-  int age = 12;
-  double money = 69.99;
-  
+  // String name = "wuyu"; // 문자열
+  // bool boolean = true; // 불타입
+  // int age = 12; // 정수타입 (num 상속)
+  // double money = 69.99; // 실수타입 (num 상속)
+
+  // num x = 12; // 숫자타입 ( 정수와 실수 모두 사용 가능 )
+  // x = 1.1;
+
+  // var testData = 5;
+  // var testData2 = "SEX";
+  // var numbers = [
+  //   1,
+  //   2,
+  //   3,
+  //   4,
+  //   if (testData == 5) testData2,
+  // ];
+  // // 위와 동일 List<int> numbers = [1, 2, 3, 4];
+  // print(numbers);
+
+  // var name = "wuyu";
+  // var age = 12;
+  // var greeting =
+  //     "hello sexman, my name is $name nice to meet you!!! my age is ${age + 1}";
+
+  // print(greeting);
+
+  // var sex = "sex";
+  // var oldFriends = ['nico', 'lunn'];
+  // var newFriends = [
+  //   'wuyu',
+  //   'pig',
+  //   'princess',
+  //   for (var friend in oldFriends) "sex $friend",
+  // ];
+  // print(newFriends);
+
+  // var player = {
+  //   'name': 'nico',
+  //   'xp': '19.99',
+  //   'superpower': 'false',
+  // };
+
+  List<Map<String, Object>> players = [
+    {'name': "nico", "xp": 199993.999},
+    {'name': "nico", "xp": 199993.999},
+  ];
+
+  print(players);
 }
