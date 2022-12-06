ba439768d439bffced8b8aa94f817d37f9c8f399
yunha <wjh0970@gmail.com>
2022-12-07 02:27:35 +0900

    javaScript test_2

--- test.js
+++ test.js
@@ -20,6 +20,21 @@
 // console.log(people.age || "noname");
 // console.log(people.age > 11 && "accept");
 
-let num = 1_000_000_000;
+// let num = 1_000_000_000;
 
-console.log(num);
+// console.log(num);
+
+function drawStar(starNum) {
+  let result = "";
+
+  for (let i = starNum-1; i >= 0; i--) {
+    result += "\n";
+    
+    for (let j = 0; j <= starNum; j++) {
+      result += "*";
+    }
+  }
+  console.log(result);
+}
+
+drawStar(10);
